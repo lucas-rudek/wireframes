2d1a4d45f38862de726d07164262fc64b4306158
Lucas Rudek <lucas-rudek@hotmail.com>
2021-01-11 15:19:09 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import "./styles.css";
 import Container from "./components/Container.jsx";
-import SideBar from "./components/SideBar.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import { AppWrapper } from "./components/styles.js";
@@ -10,7 +8,6 @@ export default function App() {
   return (
     <AppWrapper>
       <Header />
-      <SideBar />
       <Container />
       <Footer />
     </AppWrapper>
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MainWrapper } from "./styles.js";
+
+export default function Main() {
+  return (
+    <MainWrapper>
+      <h3>MAIN</h3>
+    </MainWrapper>
+  );
+}
