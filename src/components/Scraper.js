9e113f05d0db734010deeb5ae2bbd036a9ed7db5
Lucas Rudek <lucas-rudek@hotmail.com>
2021-01-23 14:52:51 -0300

    scraped

--- src/components/Scraper.js
+++ src/components/Scraper.js
@@ -1,13 +1,10 @@
 import axios from "axios";
-import cheerio from "cheerio";
 
 export default async function Scraper() {
-  const url = "https://example.com/";
+  const url =
+    "https://api.scraperapi.com/?api_key=69721ee4e521c5738c2d9059e6a15e69&url=https://pr.olx.com.br/regiao-de-ponta-grossa-e-guarapuava/autos-e-pecas/carros-vans-e-utilitarios/bmw-118i-sport-831618601";
 
-  async function fetchHTML(url) {
-    const { data } = await axios.get(url);
-    return cheerio.load(data);
-  }
-  const $ = await fetchHTML(url);
-  console.log(`Site HTML: ${$.html()}\n\n`);
+  const teste = await axios.get(url);
+
+  console.log(teste.data);
 }
