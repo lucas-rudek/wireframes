83d0b6b995a947065c4b779e1defaf9e9245ed55
Lucas Rudek <lucas-rudek@hotmail.com>
2021-01-26 17:34:12 -0300

    working

--- src/components/Scraper.js
+++ src/components/Scraper.js
@@ -1,24 +1,20 @@
-import axios from "axios";
 import cheerio from "cheerio";
 import request from "request";
 
 export default async function Scraper() {
   const url =
-    "https://app.scrapingbee.com/api/v1/?api_key=AP91PR5CN47N6B8KRFX51XBWRBMGMRHDONI0BIZX622C8BTG5U5NRDM6L9H9XZTVVYXDO8O5M0ER8TKB&url=https%3A%2F%2Fpr.olx.com.br%2Fregiao-de-ponta-grossa-e-guarapuava%2Fautos-e-pecas&render_js=false";
+    "https://app.scrapingbee.com/api/v1/?api_key=AP91PR5CN47N6B8KRFX51XBWRBMGMRHDONI0BIZX622C8BTG5U5NRDM6L9H9XZTVVYXDO8O5M0ER8TKB&url=https%3A%2F%2Fpr.olx.com.br%2Fregiao-de-ponta-grossa-e-guarapuava%2Fautos-e-pecas%2Fcarros-vans-e-utilitarios&render_js=false";
 
-  request(
-    {
-      method: "GET",
-      url: url
-    },
-    (err, res, body) => {
-      if (err) return console.error(err);
+  request({ url: url }, (err, res, body) => {
+    if (err) return console.error(err);
+    let $ = cheerio.load(body);
 
-      let $ = cheerio.load(body);
+    /*const data = $.map((res, index) => ({
+      title: $(".sc-1iuc9a2-0 h2").text(),
+      price: $(".sc-hmzhuo p").text(),
+      img: $(".sc-101cdir-2 img").attr("src")
+    }));*/
 
-      let title = $("title");
-
-      console.log(title.text());
-    }
-  );
+    let teste = $(".sc-1iuc9a2-0 h2").map((res) => console.log(res));
+  });
 }
